2887b32ea83067b2e05cccb402b1a3b8a0d60259
test <test>
2023-10-09 18:06:54 +0700

    test

---
+++ faces.py
@@ -0,0 +1,12 @@
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    return text
+
+def main():
+    text = input()
+    converted_text = convert(text)
+    print(converted_text)
+
+if __name__ == "__main__":
+    main()
